b2b362688cfac8fa5aa6225f1d7a0a059ef8eb41
Ricardo Gallegos <ricardo.gallegos@usach.cl>
2025-07-23 19:21:54 -0400

    correccion de metodos y validaciones

--- src/prevencionRiesgos/Usuario.java
+++ src/prevencionRiesgos/Usuario.java
@@ -8,43 +8,88 @@ public class Usuario implements Asesoria{
 	private int run;
 	
 	public Usuario() {
-		this.nombre = null;
-		this.fechaNacimiento = LocalDate.of(1900, 1, 1);
-		this.run = 0;
+		setNombre(null);
+		setFechaNacimiento( LocalDate.of(1900, 1, 1) );
+		setRun(0);
 	}
 	
 	public Usuario(String nombre, LocalDate fechaNacimiento, int run) {
-		this.nombre = nombre;
-		this.fechaNacimiento = fechaNacimiento;
-		this.run = run;
+		setNombre(nombre);
+		setFechaNacimiento(fechaNacimiento);
+		setRun(run);
 	}
 
 	public String toString() {
-		return String.format("%s, %s, %s", this.nombre, this.fechaNacimiento, this.run);
+		return String.format("%s, %s, %s", getNombre(), getFechaNacimiento(), getRun());
 	}
 	
 	public String mostrarEdad() {
 		return String.format("El usuario tiene %d años", calcularEdad());
 	}
 	
-	private String fecha() {
+	private boolean validarRun(int run) {
+		String FORMATO_RUN = "\\d{6,8}";
+		return Integer.toString(run).matches(FORMATO_RUN);
+	}
+	
+	private boolean validarNombre(String nombre) {
+		String FORMATO_NOMBRE = "[a-z A-Z]{10,50}";
+		return nombre.matches(FORMATO_NOMBRE);
+	}
+	
+	private boolean validarFecha() {
 	/*
 		LocalDate fecha = LocalDate.of(2000, 4, 15);
 		System.out.println(fecha);
 	 */
-		return "";
+		int dias = Period.between(getFechaNacimiento(), LocalDate.of(1900, 1, 1)).getDays();
+		return dias == 0 ? true: false;
 	}
 	
 	private int calcularEdad() {
 		//LocalDate fecha = LocalDate.now() - this.fechaNacimiento;
-		Period periodo = Period.between(this.fechaNacimiento, LocalDate.now());
-//		System.out.println(periodo.getYears());	
+		Period periodo = Period.between(getFechaNacimiento(), LocalDate.now());
+//		System.out.println(periodo.getYears());
 		return periodo.getYears();
 	}
 
 	@Override
-	public String analizarUsuario() {
-		// TODO Auto-generated method stub
-		return null;
+	public String analizarUsuario() {		
+		return String.format("Nombre: %s\nRUN: %s",getNombre(), getRun());
+	}
+//------------- Getter & Setter -----------------:start
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		if ( validarNombre(nombre) ) {			
+			this.nombre = nombre;
+		}else {
+			//invalido - se usa valor por defecto - invocar algun aviso
+			this.nombre = null;
+		}
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public int getRun() {
+		return run;
+	}
+
+	public void setRun(int run) {
+		if ( validarRun(run) ) {
+			this.run = run;
+		}else {
+			//invalido - se usa valor por defecto - invocar algun aviso
+			this.run = 0;
+		}
 	}
 }
+//------------- Getter & Setter -----------------:end
