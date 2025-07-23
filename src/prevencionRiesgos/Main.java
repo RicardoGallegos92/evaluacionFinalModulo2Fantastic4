c994070f9ed69d0ecb81daedc9872c9563279c03
Marglodis Tovar <marglodistovar@gmail.com>
2025-07-23 19:25:31 -0400

    Deshacer cambios en clases base

--- src/prevencionRiesgos/Main.java
+++ src/prevencionRiesgos/Main.java
@@ -1,31 +1,12 @@
 package prevencionRiesgos;
 
-import java.time.LocalDate;
-
 public class Main {
 
 	public static void main(String[] args) {
 		Usuario user = new Usuario();
-		
-		// Crear una fecha de nacimiento y fecha de ingreso de ejemplo
-        LocalDate fechaNacimiento = LocalDate.of(1985, 5, 15);
-        LocalDate fechaIngreso = LocalDate.of(2020, 3, 10);
-		
-     // Crear un objeto Profesional
-        Profesional profesional = new Profesional(
-            "Mar�a Gonz�lez",
-            fechaNacimiento, 
-            12345678,
-            null,
-            fechaIngreso
-        );
-        
-        // Probar el método analizarUsuario() de la clase PROFESIONAL
-        System.out.println("=== AN�LISIS DEL USUARIO PROFESIONAL ===");
-        String resultado = profesional.analizarUsuario();
-        System.out.println(resultado);
-        
-		
+		System.out.println( user );
+		System.out.println( user.mostrarEdad() );
+
 	}
 
 }
