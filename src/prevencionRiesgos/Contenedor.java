14dd8342ff03ee5fd54a7b0bb88074340bf97cc8
Ricardo Gallegos <ricardo.gallegos@usach.cl>
2025-08-07 01:27:47 -0400

    Contenedor.java - metodos almacenamiento de Asesorias

--- src/prevencionRiesgos/Contenedor.java
+++ src/prevencionRiesgos/Contenedor.java
@@ -5,26 +5,47 @@ import java.util.ArrayList;
 public class Contenedor {
 	ArrayList<Asesoria> asesorias;
 	ArrayList<Capacitacion> capacitaciones;
-	
-	
 //------------- Constructores -----------------:start
+	public Contenedor() {
+		this.asesorias = null;
+		this.capacitaciones = null;
+	}
+	
+	public Contenedor(ArrayList<Asesoria> asesorias,
+				ArrayList<Capacitacion> capacitaciones) {
+		this.asesorias = asesorias;
+		this.capacitaciones = capacitaciones;
+	}
 //------------- Constructores -----------------:end
-
 //------------- Metodos -----------------:start
-	public void AlmacenarCliente(Cliente cliente){
-		
+	@Override
+	public String toString() {
+		return "Contenedor: {asesorias=" + asesorias
+				+ ", capacitaciones=" + capacitaciones + "}";
 	}
 	
+	private void almacenarAsesoria(Asesoria asesoria) {
+		if ( asesoria != null ) {
+			this.asesorias.add(asesoria);
+		}
+	}
+
+	public void almacenarCliente(Cliente cliente){
+		almacenarAsesoria(cliente);
+	}
+
 	public void almacenarProfesional(Profesional profesional) {
-		
+		almacenarAsesoria(profesional);
 	}
 	
-	public void almacenarAdministrativo() {
-		
+	public void almacenarAdministrativo(Administrativo administrativo) {
+		almacenarAsesoria(administrativo);
 	}
 	
-	public void almacenarCapacitacion() {
-		
+	public void almacenarCapacitacion(Capacitacion capacitacion) {
+		if ( capacitacion != null ) {
+			this.capacitaciones.add(capacitacion);
+		}
 	}
 	
 	public void eliminarUsuario(int run) {
@@ -41,11 +62,11 @@ public class Contenedor {
 		return a;
 	}
 	
-	public ArrayList<Capacitacion>  () {
-		
+	public ArrayList<Capacitacion> listarCapacitaciones () {
+		ArrayList<Usuario> a = new ArrayList<>();
+		return a;		
 	}
 //------------- Metodos -----------------:end
-
 //------------- Getter & Setter -----------------:start
 //------------- Getter & Setter -----------------:end
 }
